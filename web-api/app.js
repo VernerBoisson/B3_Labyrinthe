3fd2d141588eab6db5efae6c6d5573f13f43b118
VernerBoisson <verner.boisson@ynov.com>
2019-12-03 18:38:40 +0100

    json hello world

--- web-api/app.js
+++ web-api/app.js
@@ -1,30 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-
-var app = express();
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.all('/', require('./routes/home'))
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err })
-});
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.json({1:'Hello World'})
+})
 
 module.exports = app;
