73bb2f063f1adbfd8a82c5d60787539066933112
VernerBoisson <verner.boisson@ynov.com>
2019-12-03 11:56:11 +0100

    docker mongo version fix

--- web-api/app.js
+++ web-api/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -12,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
+app.use('/', "test")
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
