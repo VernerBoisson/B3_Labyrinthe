317ed0e138f98729356cc8baa1f0fdf4a396fe36
Maelpena <34342829+Maelpena@users.noreply.github.com>
2020-03-26 20:31:39 +0100

    the real ihm edit avec post sur api

---
+++ the-real-ihm-edit/ihm-edit/src/main.java
@@ -0,0 +1,22 @@
+import GUI.MazeGUI;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+
+import javax.swing.*;
+import java.io.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.*;
+import java.net.URLConnection;
+
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        MazeGUI gui = new MazeGUI();
+
+
+
+
+    }
+}
